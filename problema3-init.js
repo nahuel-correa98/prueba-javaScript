075fae53e530e99399b586494a607ba543417255
nahuel correa <nahuel16.tigre@gmail.com>
2023-05-08 13:50:18 -0300

    problema3

---
+++ problema3-init.js
@@ -0,0 +1,13 @@
+class user{
+
+constructor (name, email, movies)
+
+this.name = name
+this.email = email
+this.movies = movies
+	
+}
+
+const myUser = new user(Juan Perez, juanperez23@gmail.com, Titamic)
+
+console.log myUser
