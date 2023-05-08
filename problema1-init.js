88f1bc440771b443b52ca39f156c1d928144dd09
nahuel correa <nahuel16.tigre@gmail.com>
2023-05-08 13:48:18 -0300

    problemat

---
+++ problema1-init.js
@@ -0,0 +1,8 @@
+let number = parseInt(prompt("Enter a number please:")); 
+
+     if ( number > 0 && number % 5 == 0 ) {
+         console.log("the number"+number+ " is multiple of 3"  )
+     }else{
+       console.log("the number "+number+ "not is multiple of 3");
+
+      }
